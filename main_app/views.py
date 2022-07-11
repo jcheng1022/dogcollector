5a48b48e67e7e191a4b7499178c15852a8361af1
Jacky <jacky@Jackys-MBP.fios-router.home>
2022-07-10 20:44:59 -0400

    adds authentication/authorization and updating code for heroku

--- main_app/views.py
+++ main_app/views.py
@@ -7,6 +7,10 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView
 from django.views.generic import DetailView
 from .forms import PlayedForm
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def home(request):
     return render(request, 'home.html')
@@ -16,11 +20,11 @@ def about(request):
     return render(request, 'about.html')
 
 
-
+@login_required
 def dogs_index(request):
-    dogs = Dog.objects.all()
+    dogs = Dog.objects.filter(user=request.user)
     return render(request,'dogs/index.html', {'dogs':dogs})
-
+@login_required
 def dogs_details(request,dog_id):
     dog = Dog.objects.get(id=dog_id)
     played_form = PlayedForm()
@@ -28,7 +32,7 @@ def dogs_details(request,dog_id):
     played_form = PlayedForm()
     return render(request, 'dogs/details.html', {'dog': dog, 'played_form':played_form, 'toys':toys_dog_doesnt_have})
 
-class DogCreate(CreateView):
+class DogCreate(LoginRequiredMixin, CreateView):
     model=Dog
     fields=['name', 'breed', 'description', 'age']
     def form_valid(self, form):
@@ -36,15 +40,15 @@ class DogCreate(CreateView):
         return super().form_valid(form)
 
 
-class DogUpdate(UpdateView):
+class DogUpdate(LoginRequiredMixin, UpdateView):
     model=Dog
     fields=['breed','description', 'age']
 
-class DogDelete(DeleteView):
+class DogDelete(LoginRequiredMixin, DeleteView):
     model=Dog
 
 
-
+@login_required
 def add_playing(request, dog_id):
     form = PlayedForm(request.POST)
     if form.is_valid():
@@ -53,33 +57,48 @@ def add_playing(request, dog_id):
         new_playing.save()
     return redirect('detail', dog_id=dog_id)
 
-
+@login_required
 def assoc_toy(request, dog_id, toy_id):
     Dog.objects.get(id=dog_id).toys.add(toy_id)
     return redirect('detail', dog_id = dog_id)
-
+@login_required
 def assoc_toy_del(request, dog_id, toy_id):
     Dog.objects.get(id=dog_id).toys.remove(toy_id)
     return redirect('detail', dog_id = dog_id)
 
 
-class ToyList(ListView):
+class ToyList(LoginRequiredMixin, ListView):
     model = Toy
     template_name = 'toys/index.html'
 
 
-class ToyDetail(DetailView):
+class ToyDetail(LoginRequiredMixin, DetailView):
     model = Toy
     template_name = 'toys/detail.html'
 
-class ToyCreate(CreateView):
+class ToyCreate(LoginRequiredMixin, CreateView):
     model = Toy
     fields= ['name','color']
 
-class ToyUpdate(UpdateView):
+class ToyUpdate(LoginRequiredMixin, UpdateView):
     model = Toy
     fields = ['name', 'color']
 
-class ToyDelete(DeleteView):
+class ToyDelete(LoginRequiredMixin, DeleteView):
     model = Toy
     success_url = '/toys/'
+
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
